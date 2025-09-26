a6421f4fd47a6c82b7f9951e47ac128bea8f25ca
DinilKodituwakku <dinilkodiz@gmail.com>
2025-09-26 18:36:23 +1000

    Added a newsletter checkbox check and sign disabled test

---
+++ my-app/src/test/newsletter-defaut.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+function NewsletterOptIn() {
+  return (
+    <label>
+      <input
+        type="checkbox"
+        aria-label="Subscribe to newsletter"
+        defaultChecked={false}
+      />
+      Subscribe to our newsletter
+    </label>
+  );
+}
+
+test("newsletter checkbox is unchecked by default", () => {
+  render(<NewsletterOptIn />);
+  const cb = screen.getByLabelText(
+    /subscribe to newsletter/i
+  ) as HTMLInputElement;
+  expect(cb.checked).toBe(false);
+});
