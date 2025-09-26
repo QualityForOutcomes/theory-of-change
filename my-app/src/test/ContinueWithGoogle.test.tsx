c3a6ec572f14df7e2ffacee7cad5570e3a6131bd
DinilKodituwakku <dinilkodiz@gmail.com>
2025-09-26 18:41:24 +1000

    Continue with google and forgot password

---
+++ my-app/src/test/ContinueWithGoogle.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+function GoogleCTA() {
+  return <button type="button">Continue with Google</button>;
+}
+
+test("renders Google auth button", () => {
+  render(<GoogleCTA />);
+  expect(
+    screen.getByRole("button", { name: /continue with google/i })
+  ).toBeInTheDocument();
+});
